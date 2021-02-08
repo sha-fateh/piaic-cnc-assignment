24943458cdab78bedd2476c602d3c310df487e0e
sha-fateh <fatehullah.jalbani@gmail.com>
2021-02-09 00:09:27 +0500

    chapters1 to 7

---
+++ js_assignments/chap_1_2_3/app.js
@@ -0,0 +1,4 @@
+var firstName = "Fatehullah";
+var lastName = "Jalbani";
+var age = 32;
+alert("Hello "+firstName+" "+lastName+","+ " you are "+age+" years old.")
---
+++ js_assignments/chap_5_6_7/app.js
@@ -0,0 +1,5 @@
+var a = 21;
+var b = a++ + ++a - a-- + --a;
+
+alert("The value of a is: "+a);
+alert("The value of b is: "+b);
